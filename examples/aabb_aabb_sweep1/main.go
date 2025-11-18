76fb6484100ccbd21ec8cbd15064462d73edc06b
Barış <duurkak@yandex.com>
2025-11-18 23:59:16 +0300

    replace aabb_aabb_sweep1 example

--- examples/aabb_aabb_sweep1/main.go
+++ examples/aabb_aabb_sweep1/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"math"
 
 	"github.com/setanarut/coll"
 	"github.com/setanarut/coll/examples"
 	"github.com/setanarut/v"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"golang.org/x/image/colornames"
 )
 
 var (
-	box            = coll.NewAABB(0, 0, 16, 16)
-	wall           = coll.NewAABB(250, 250, 16*4, 16)
-	hit            = &coll.HitInfo{}
-	velocity       = v.Vec{}
-	cursorPosition = v.Vec{}
-	slidingEnabled bool
-	collided       bool
+	angle, factor float64
+	staticBox     = coll.NewAABB(250, 250, 16*7, 16)
+	sweepBoxes    = [2]*coll.AABB{
+		coll.NewAABB(98, 274, 16, 16),
+		coll.NewAABB(128+250, -48+250, 16, 16),
+	}
+	tempBoxes = [2]*coll.AABB{
+		coll.NewAABB(250, 250, 16, 16),
+		coll.NewAABB(250, 250, 16, 16),
+	}
+	sweepDeltas = [2]v.Vec{{64, -12}, {-32, 96}}
+	deltas      = [2]v.Vec{}
+
+	hitInfos = [2]*coll.HitInfo{{}, {}}
+	sweeps   [2]bool
 )
 
 func main() {
@@ -36,51 +42,42 @@ type Game struct {
 }
 
 func (g *Game) Update() error {
-	cursorPosition = examples.CursorPos()
+	angle += 0.5 * math.Pi * 0.02
+	factor = math.Max((math.Cos(angle)+1)*0.5, 1e-8)
 
-	if inpututil.IsKeyJustPressed(ebiten.KeyTab) {
-		slidingEnabled = !slidingEnabled
-	}
-
-	velocity = cursorPosition.Sub(box.Pos)
-
-	hit.Reset()
-	collided = coll.AABBAABBSweep1(wall, box, velocity, hit)
-
-	if collided {
-		if slidingEnabled {
-			slideVel := coll.CalculateSlideVelocity(velocity, hit) // Sliding correction
-			box.Pos = box.Pos.Add(slideVel)
-		} else {
-			newVel := velocity.Scale(hit.Time) // or velocity.Add(hit.Delta)
-			box.Pos = box.Pos.Add(newVel)
-		}
-	} else {
-		box.Pos = box.Pos.Add(velocity)
+	for i, box := range sweepBoxes {
+		deltas[i] = sweepDeltas[i].Scale(factor)
+		hitInfos[i].Reset()
+		sweeps[i] = coll.AABBAABBSweep1(staticBox, box, deltas[i], hitInfos[i])
 	}
 	return nil
 }
 
 func (g *Game) Draw(s *ebiten.Image) {
-	examples.StrokeAABB(s, wall, colornames.Gray)
-	if collided {
-		examples.StrokeAABB(s, box, colornames.Yellow)
-		examples.DrawHitNormal(s, hit, colornames.Yellow, false)
-	} else {
-		examples.StrokeAABB(s, box, colornames.Green)
+
+	examples.StrokeAABB(s, staticBox, colornames.Gray)
+
+	for i, box := range sweepBoxes {
+		examples.StrokeAABB(s, box, colornames.Gray)
+		if sweeps[i] {
+			// Draw a red box at the point where it was trying to move to
+			examples.DrawRay(s, box.Pos, deltas[i].Unit(), deltas[i].Mag(), colornames.Red, true)
+			tempBoxes[i].Pos = box.Pos.Add(deltas[i])
+
+			examples.StrokeAABB(s, tempBoxes[i], colornames.Red)
+
+			// Draw a yellow box at the point it actually got to
+			tempBoxes[i].Pos = box.Pos.Add(deltas[i].Scale(hitInfos[i].Time))
+			examples.StrokeAABB(s, tempBoxes[i], colornames.Yellow)
+			examples.DrawHitNormal(s, hitInfos[i], colornames.Yellow, false)
+
+		} else {
+			tempBoxes[i].Pos = box.Pos.Add(deltas[i])
+			examples.StrokeAABB(s, tempBoxes[i], colornames.Green)
+			examples.DrawRay(s, box.Pos, deltas[i].Unit(), deltas[i].Mag(), colornames.Green, true)
+		}
+
 	}
-	examples.PrintHitInfoAt(s, hit, 10, 100)
-	ebitenutil.DebugPrintAt(
-		s,
-		fmt.Sprintf(
-			"Sliding Enabled: %v (Press Tab)\nVel: %v\nBoxPos: %v",
-			slidingEnabled,
-			velocity,
-			box.Pos,
-		),
-		10,
-		10,
-	)
 }
 
 func (g *Game) Layout(w, h int) (int, int) {
